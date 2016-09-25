57f1ec71460566c41bd7a9badb626955f192e3e6
Sanqiang <zhaosanqiang916@gmail.com>
2016-09-25 00:35:05 -0400

    add hashtable sparsetable

---
+++ train/util/helper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+char* concat(char *s1, char *s2)
+{
+    char *result = malloc((strlen(s1)+strlen(s2)+1)* sizeof(char));//+1 for the zero-terminator
+    //in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+float * expTable;
+void init_util(){
+    //populate exp precomputing table
+    int i;
+    expTable = (float *)malloc((EXP_TABLE_SIZE + 1) * sizeof(float));
+    for (i = 0; i < EXP_TABLE_SIZE; i++) {
+        expTable[i] = exp((i / (float)EXP_TABLE_SIZE * 2 - 1) * MAX_EXP); // Precompute the exp() table
+        expTable[i] = expTable[i] / (expTable[i] + 1);                   // Precompute f(x) = x / (x + 1)
+    }
+}
